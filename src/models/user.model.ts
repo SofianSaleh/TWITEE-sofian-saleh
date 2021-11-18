2b6a4075ff9001dbdc146e0be02f4f3db778c587
SofianSaleh <sofiane.saleh11@gmail.com>
2021-11-18 15:57:38 +0200

    :fire:

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,13 +1,13 @@
-import { model, Schema, Document, HookNextFunction } from 'mongoose';
-import { hash, compare } from 'bcrypt';
+import { model, Schema, Document } from 'mongoose';
+import { hashSync, compare, genSalt } from 'bcrypt';
 import config from 'config';
 
 export interface UserDocument extends Document {
   email: string;
   name: string;
   password: string;
-  createdAt: date;
-  updatedAt: date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const UserSchema = new Schema(
@@ -20,9 +20,24 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
-UserSchema.pre('save', async (next: HookNextFunction) => {
-    if(this.isModified)
+UserSchema.pre('save', async (next) => {
+  let user = this as UserDocument;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  const salt = await genSalt(config.get<number>('saltWorkFactor'));
+  const hash = await hashSync(user.password, salt);
+  user.password = hash;
+  return next();
 });
+
+UserSchema.methods.comparePassword = async (
+  candidatePassword: string
+): Promise<boolean> => {
+  let user = this as UserDocument;
+
+  return compare(candidatePassword, user.password).catch((e) => false);
+};
 
 const UserModel = model<UserDocument>('User', UserSchema);
 
