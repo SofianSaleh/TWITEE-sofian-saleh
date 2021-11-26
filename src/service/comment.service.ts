e0a0cc03e53c5babce2b227d2040a1da22732ef1
SofianSaleh <sofiane.saleh11@gmail.com>
2021-11-26 23:19:47 +0200

    :fire: added the update Tweet

--- src/service/comment.service.ts
+++ src/service/comment.service.ts
@@ -86,17 +86,20 @@ export const deleteComment = async (commentId: string, user: any) => {
     // Check if the id of the comment owner is the same as the one who wants to delete it
     if (user._id != comment.data.owner._id)
       return { success: false, comment: null, msg: `Forbidden` };
+    await comment.data.remove();
+    await comment.data.save();
+    console.log(comment.data);
 
-    const deleteComment = await CommentModel.deleteOne({
-      _id: comment.data._id,
-    });
-    console.log({
-      success: true,
-      data: deleteComment.deletedCount,
-      msg: deleteComment.deletedCount
-        ? `Deleted Successfully`
-        : 'Something wrong',
-    });
+    // const deleteComment = await CommentModel.deleteOne({
+    //   _id: comment.data._id,
+    // });
+    // console.log({
+    //   success: true,
+    //   data: deleteComment.deletedCount,
+    //   msg: deleteComment.deletedCount
+    //     ? `Deleted Successfully`
+    //     : 'Something wrong',
+    // });
   } catch (e: any) {
     throw e;
   }
