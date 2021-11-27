d51ab074f4194609c65003ca126a42273ee9ec29
SofianSaleh <sofiane.saleh11@gmail.com>
2021-11-27 13:56:32 +0200

    :fire: started documenting the api

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -13,7 +13,40 @@ import {
   updateCommentHandler,
 } from '../controller/comment.controller';
 const router = express.Router();
-
+/**
+ * @openapi
+ * '/api/comment/create/{id}':
+ *  post:
+ *     tags:
+ *     - Comment
+ *     summary: Comment on  a tweet
+ *     parameters:
+ *       - name: x-refresh
+ *         in: header
+ *         descritpion: A refresh token that was created when you login
+ *         require: true
+ *         type: string
+ *       - name: id
+ *         in: path
+ *         descritpion: Tweet id
+ *         require: true
+ *         type: string
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/CreateTweetInput'
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CommentsResponse'
+ *      400:
+ *        description: Bad request
+ */
 router.post(
   '/create/:id',
   requireUser,
--- src/schema/comment.schema.ts
+++ src/schema/comment.schema.ts
@@ -1,5 +1,60 @@
 import { object, string } from 'zod';
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    CommentsResponse:
+ *      type: object
+ *      properties:
+ *        content:
+ *          type: string
+ *        likedUser:
+ *          items:
+ *            type: string
+ *        comments:
+ *          type: array
+ *          items:
+ *            $ref: '#/components/schemas/CommentSchema'
+ *        _id:
+ *          type: string
+ *        owner:
+ *          $ref: '#/components/schemas/OwnerSchema'
+ *        isEdited:
+ *          type: boolean
+ *        createdAt:
+ *          type: string
+ *        updatedAt:
+ *          type: string
+ *        __v:
+ *          type: integer
+ *    OwnerSchema:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *        name:
+ *          type: string
+ *        email:
+ *          type: string
+ *    CommentSchema:
+ *      type: object
+ *      properties:
+ *        content:
+ *          type: string
+ *        owner:
+ *          type: string
+ *        isEdited:
+ *          type: boolean
+ *        _id:
+ *          type: string
+ *        updatedAt:
+ *          type: string
+ *        createdAt:
+ *          type: string
+ *        __v:
+ *          type: integer
+ */
 export const createCommentSchema = object({
   body: object({
     content: string({ required_error: 'Email is required' }),
